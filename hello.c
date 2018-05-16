3b4646805598397fb2dc7314945d4e4df7aebe80
venki <venki.linux7@gmail.com>
2018-05-16 20:27:22 +0530

    Small change

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@ int main()
 {
 <<<<<<< HEAD
 <<<<<<< HEAD
-   //printf() displays the string inside quotation
+   //printf () displays the string inside quotation
 =======
    //  printf() displays the string inside quotation
 >>>>>>> 5ea21a8129a09d05527d8cddc83d509d8ec236e2
